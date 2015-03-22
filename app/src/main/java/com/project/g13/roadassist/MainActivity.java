8ed9111b40f50723c8a153f2165699064013f1d8
perskytt <perskytt@outlook.com>
2015-03-22 22:45:55 +0100

    Added a map view and GUI, very simple stuff

--- app/src/main/java/com/project/g13/roadassist/MainActivity.java
+++ app/src/main/java/com/project/g13/roadassist/MainActivity.java
@@ -1,18 +1,44 @@
 package com.project.g13.roadassist;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button plnRtBtn = (Button)findViewById(R.id.plnRtBtn);
+        Button vwRtBtn = (Button)findViewById(R.id.vwRtBtn);
+
+        plnRtBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
+        vwRtBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
     }
+
+
+    public void click(View view) {
+    }
+
 
 
     @Override
---
+++ app/src/main/java/com/project/g13/roadassist/Plan_Route.java
@@ -0,0 +1,9 @@
+package com.project.g13.roadassist;
+
+/**
+ * Created by Per on 2015-03-12.
+ */
+public class Plan_Route {
+
+    //TODO A view for input of route and such
+}
