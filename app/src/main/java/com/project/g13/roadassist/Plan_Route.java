e3e0eb23b311515e5b88319320e102c76f427f00
perskytt <perskytt@outlook.com>
2015-05-27 11:45:55 +0200

    Fixed the getAddressFromLatLng method

--- app/src/main/java/com/project/g13/roadassist/Plan_Route.java
+++ app/src/main/java/com/project/g13/roadassist/Plan_Route.java
@@ -278,14 +278,21 @@ public class Plan_Route extends ActionBarActivity implements
     }
 
 
-    // Method for getting latitude and longtitude from an adress sent to google geocode webservice
-    public static void getLatLongFromAddress(String youraddress) {
+    /*
+     Method for getting latitude and longtitude from an adress sent to google geocode webservice
+     @param youraddress input string search query for Google Places-address search
+     @return string arrray with lat[0] and lng[1] coordinates
+      */
+    public static String[] getLatLongFromAddress(String youraddress) {
 
-
+        String[] Coordinates = null;
         double lng;
         double lat;
+        String formatted = youraddress.replace(" ", "+");
+        formatted = formatted.replaceAll(",","");
+
         String uri = "http://maps.google.com/maps/api/geocode/json?address=" +
-                youraddress + "&sensor=false";
+                formatted + "&sensor=false";
         HttpGet httpGet = new HttpGet(uri);
         HttpClient client = new DefaultHttpClient();
         HttpResponse response;
@@ -321,10 +328,15 @@ public class Plan_Route extends ActionBarActivity implements
 
             Log.d("latitude", latitude);
             Log.d("longitude", longtitude);
+
+            Coordinates[0]= latitude;
+            Coordinates[1]= longtitude;
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
+        if(Coordinates!=null){
+return Coordinates;}
+        else return null;
     }
 
 
@@ -334,7 +346,7 @@ public class Plan_Route extends ActionBarActivity implements
 
 
 
-// And the corresponding Adapter
+// the Adapter for the Autocompletetextview and Google Places API
 class AutoCompleteAdapter extends ArrayAdapter<AutoCompleteAdapter.PlaceAutocomplete> implements Filterable {
 
 
@@ -465,88 +477,3 @@ class AutoCompleteAdapter extends ArrayAdapter<AutoCompleteAdapter.PlaceAutocomp
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    private LayoutInflater mInflater;
-    private Geocoder mGeocoder;
-    private StringBuilder mSb = new StringBuilder();
-    public AutoCompleteAdapter(final Context context) {
-        super(context, -1);
-        mInflater = LayoutInflater.from(context);
-        mGeocoder = new Geocoder(context);
-    }
-    @Override
-    public View getView(final int position, final View convertView, final ViewGroup parent) {
-        final TextView tv;
-        if (convertView != null) {
-            tv = (TextView) convertView;
-        } else {
-            tv = (TextView) mInflater.inflate(android.R.layout.simple_dropdown_item_1line, parent, false);
-        }
-        tv.setText(createFormattedAddressFromAddress(getItem(position)));
-        return tv;
-    }
-    private String createFormattedAddressFromAddress(final Address address) {
-        mSb.setLength(0);
-        final int addressLineSize = address.getMaxAddressLineIndex();
-        for (int i = 0; i < addressLineSize; i++) {
-            mSb.append(address.getAddressLine(i));
-            if (i != addressLineSize - 1) {
-                mSb.append(", ");
-            }
-        }
-        return mSb.toString();
-    }
-    @Override
-    public Filter getFilter() {
-        Filter myFilter = new Filter() {
-            @Override
-            protected FilterResults performFiltering(final CharSequence constraint) {
-                List<Address> addressList = null;
-                if (constraint != null) {
-                    try {
-                        addressList = mGeocoder.getFromLocationName((String) constraint, 5);
-                    } catch (IOException e) {
-                    }
-                }
-                if (addressList == null) {
-                    addressList = new ArrayList<Address>();
-                }
-                final FilterResults filterResults = new FilterResults();
-                filterResults.values = addressList;
-                filterResults.count = addressList.size();
-                return filterResults;
-            }
-            @SuppressWarnings("unchecked")
-            @Override
-            protected void publishResults(final CharSequence contraint, final FilterResults results) {
-                clear();
-                for (Address address : (List<Address>) results.values) {
-                    add(address);
-                }
-                if (results.count > 0) {
-                    notifyDataSetChanged();
-                } else {
-                    notifyDataSetInvalidated();
-                }
-            }
-            @Override
-            public CharSequence convertResultToString(final Object resultValue) {
-                return resultValue == null ? "" : ((Address) resultValue).getAddressLine(0);
-            }
-        };
-        return myFilter;
-    }
-}
-*/
